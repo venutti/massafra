e53fb0c7a2cfb326771cafb0b32a3fb7aeb505a1
venutti <lvenutti@fi.uba.ar>
2023-02-07 16:04:39 -0300

    Arregal bug con 100vh

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const createHeightVar = () => {
+  document
+    .querySelector(":root")
+    .style.setProperty("--vh", window.innerHeight / 100 + "px");
+};
+
+createHeightVar();
+
+window.addEventListener("resize", () => {
+  createHeightVar();
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
